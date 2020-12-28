c5b594f7cf80403087a5cca5c84c63343d514525
GAYATHRI . S <66509398+GAYATHRI-3200@users.noreply.github.com>
2020-12-28 13:53:53 +0530

    Update autoreply.py

--- store/autoreply.py
+++ store/autoreply.py
@@ -8,20 +8,20 @@ from email import encoders
 def autoreply(toaddr):
 	"""SEnd reply to contact submission."""
 
-	fromaddr = "subbusubramani1995@gmail.com"
+	fromaddr = "XXXX@gmail.com"
 	msg = MIMEMultipart()
 
 	msg['From'] = fromaddr
 	msg['To'] = toaddr
 	msg['Subject'] = "Contact django reply by admin(arbaz)"
 
-	body = "Thanks for contacting us. We will get back to you after processing your submission.   MOHAMMED ARBAZ Z B,admin, ONLINE GADGETS SHOPPING Django Project"
+	body = "Thanks for contacting us. We will get back to you after processing your submission.   Miss Gayathri S,admin, ONLINE GADGETS SHOPPING Django Project"
 
 	msg.attach(MIMEText(body, 'plain'))
 
 	server = smtplib.SMTP('smtp.gmail.com', 587)
 	server.starttls()
-	server.login(fromaddr, "gayathri3200")#"PASSWORD FOR FROMADDR")
+	server.login(fromaddr, "XXXX")#"PASSWORD FOR FROMADDR")
 	text = msg.as_string()
 	server.sendmail(fromaddr, toaddr, text)
 	server.quit()
